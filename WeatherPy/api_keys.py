886688356d0618b2cf1239cf6f763aa12e9d05d7
Lisa <caruana.lisaann@gmail.com>
2021-01-27 17:39:30 -0700

    second commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c9fb60baea4a0d36add4201be947f8a4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBPoPKFkso5m4bBEQnqlJKoeGYuqqFH7o0"
