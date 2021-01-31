71c6aed884c5df95b62ed6617197d573ab2066c0
Lisa <caruana.lisaann@gmail.com>
2021-01-30 22:38:11 -0700

    Final Commit

--- Vacation Py/api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-# OpenWeatherMap API Key
-weather_api_key = "c9fb60baea4a0d36add4201be947f8a4"
-# response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=mataura&APPID=ad96356abc14d3e64bb70fbd24c88cd5&units=imperial").json()
-
-# Google API Key
-g_key = "AIzaSyBPoPKFkso5m4bBEQnqlJKoeGYuqqFH7o0"
